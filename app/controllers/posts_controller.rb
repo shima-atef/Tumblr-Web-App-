05cccfeb9e528ab828ae94f8b5a0d406ef43ff44
shima-atef <shimaatif731@gmail.com>
2023-03-29 14:17:56 +0200

    Add Validation to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,18 @@ class PostsController < ApplicationController
         @posts = Post.all.order('created_at DESC')
     end
     def new
+        @post = Post.new
     end
     
     def create
         @post = Post.new(post_params)
-        @post.save
+      
+        if  @post.save
+            redirect_to @post
+           else
+            render 'new'
+           end
 
-        redirect_to @post 
     end
     def show
         @post= Post.find(params[:id])
