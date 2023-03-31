0a3f6115e4a702e4d7cf9a4470f8af3a786b94f2
shima-atef <shimaatif731@gmail.com>
2023-03-31 20:18:57 +0200

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
 end
